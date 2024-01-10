9bb388731a87434f2a10a84b09146bc98fb348d8
perez <thangaparesh028@gmail.com>
2024-01-10 10:22:10 +0530

    new file

---
+++ src/test/java/com/taskOne/TaskOne.java
@@ -0,0 +1,18 @@
+package com.taskOne;
+
+public class TaskOne {
+
+	private void one() {
+		System.out.println("One");
+
+	}
+	private void two() {
+		System.out.println("Two");
+
+	}
+	private void three() {
+		System.out.println("Three");
+
+	}
+	
+}
---
+++ src/test/java/com/taskOne/TaskTwo.java
@@ -0,0 +1,15 @@
+package com.taskOne;
+
+public class TaskTwo {
+
+	private void hundred() {
+		System.out.println("Hundred");
+
+	}
+	private void twoHundred() {
+		System.out.println("TwoHundred");
+	}
+	private void threeHundred() {
+		System.out.println("ThreeHundred");
+	}
+}
